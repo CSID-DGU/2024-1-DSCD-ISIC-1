d6bdec231afb6510b3d09a5a8532c7a55a361b22
sayyyho <323psh@naver.com>
2024-05-25 20:13:43 +0900

    Chore: vercel 설정 수정

--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE,
+  baseURL: "/api",
   timeout: 3000,
   headers: { "Content-Type": "application/json" },
 });
